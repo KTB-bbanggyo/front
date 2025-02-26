dd6e28e4292c468a5919467b96097c561b920610
bananackck <emilly03141221@gmail.com>
2025-02-26 20:11:58 +0900

    feat : header fix & add font css file & add theme.css file theme.css 는 컬러팔레트

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../assets/styles/Header.css";
 import bread1 from "../assets/images/bread1.png"; // 이미지 import
+import bread2 from "../assets/images/bread2.png"; // 이미지 import
 
 const Header = () => {
   return (
@@ -17,7 +18,11 @@ const Header = () => {
         </div>
         <div className="header-right">
           {/* 우측 원형(프로필 등) */}
-          <div className="circle" />
+          <img
+            className="circle"
+            src={bread2}// 빵 아이콘 위치
+            alt="빵교 로고"
+          />
         </div>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
 import App from './components/App';
+import process from 'process';
+window.process = process;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
