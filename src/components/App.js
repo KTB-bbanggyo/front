c27f90f0a4e02dc3ef4237ba1511b6dc587fb2d2
doneni <xxerrenn@gmail.com>
2025-02-26 16:51:08 +0900

    feat : Add MainPage Pages and Attach to '/'

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,15 @@
-import logo from '../assets/images/bread1.png';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import '../assets/styles/App.css';
+
+import MainPage from '../pages/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>Main Page</h1>
+      <p>This is a dummy main page for the project.</p>
+    </div>
+  );
+};
+
+export default MainPage;
