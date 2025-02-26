e444717509908433a9c1f37d1502c4dd55d6289d
bananackck <emilly03141221@gmail.com>
2025-02-26 22:01:18 +0900

    fix : npm install process bug fix

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
 import App from './components/App';
-import process from 'process';
-window.process = process;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
