54c1862fd82a07d897436985e4d175529d6b3842
bananackck <emilly03141221@gmail.com>
2025-02-27 18:48:45 +0900

    feat : edit mypage

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -1,63 +1,37 @@
 import React, { useState, useEffect } from "react";
 import "../assets/styles/MyCard.css";
 import dummy from "../assets/dummy.json";
-import TagList from "./TagList";
 
 const MyCard = () => {
-  // bakery 인덱스를 상태로 관리
-  const [bakeryIdx, setBakeryIdx] = useState(0);
-  // bakery 데이터를 상태로 관리
-  const [bakery, setBakery] = useState(dummy.bakery[0].info[0]);
-//   // 각 빵집의 좋아요 상태를 저장 (키: bakery.id, 값: boolean)
-//   const [likedStates, setLikedStates] = useState({});
-//   // 좋아요 상태 변경 여부를 상태로 관리
-//   const [likedChange, setLikedChange] = useState(false);
+  const bakeries= dummy.bakery;
 
-
-  useEffect(() => { 
-    // 하트 버튼의 DOM 요소 선택
-    const heartButton = document.querySelector(".heart-btn");
-    if (likedStates[bakeryIdx]) {
-      heartButton.classList.add("liked");
-    } else {
-      heartButton.classList.remove("liked");
-    }
-  }, [likedChange]);
-
-  // 하트 버튼 클릭 시 현재 bakery의 좋아요 상태 토글
-  const toggleLiked = () => {
-    setLikedStates((prev) => ({
-      ...prev,
-      [bakeryIdx]: !prev[bakeryIdx],
-    }));
-
-    setLikedChange((prev) => ({
-      likedChange: !prev.likedChange,
-    }));
-  };
+  // 하트 버튼 클릭 시 좋아요 표시
+  const [liked, setLiked] = useState(false);
+  const toggleLiked = () => setLiked(prev => !prev);
 
   return (
-    <div className="left-content">
-      {/* 메인 콘텐츠 영역 */}
-      <main>
-        <div className="main-card">
-          {/* 상단 하트 버튼 */}
-          <button
-            className={`heart-btn`}
-            onClick={toggleLiked}
-          ></button>
-          
-          {/* 빵집 정보 */}
-          <h2 className="bakery-name">{bakery.name}</h2>
-          
-          {/* 태그 목록 */}
-          <p className="bakery-menu">이런 곳이에요!</p>
-          <TagList bakery={bakery} />
+    <div className="my-list">
+    {bakeries.map((bakery) => (
+      <div className="my-card" key={bakery.info[0].id}>
+        <div className="my-card-info">
+          <span className="my-bakery-name">{bakery.info[0].name}</span>
+          <div className="my-bakery-tags">
+            {bakery.info[0].tags.map((tag, idx) => (
+              <span className="my-tag" key={idx}>
+                {tag}
+              </span>
+            ))}
+          </div>
         </div>
-        
 
-      </main>
-    </div>
+        {/* 좋아요(하트) 버튼 */}
+        <button
+            className={`my-heart-btn ${liked ? "liked" : ""}`}
+            onClick={toggleLiked}
+          />
+      </div>
+    ))}
+  </div>
   );
 };
 
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header.jsx';
+import MyCard from '../components/MyCard.jsx';
 // import MainCard from '../components/MainCard.jsx';
 // import MapComponent from '../components/MapComponent.jsx';
 import "../assets/styles/MyPage.css";
@@ -11,7 +12,7 @@ const MyPage = () => {
       <Header />
       {/* 본문 레이아웃: 왼쪽 영역(베이지 박스), 오른쪽 지도 */}
       <div className="content-layout">
-
+        <MyCard />
 
       </div>
     </div>
