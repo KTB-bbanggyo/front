d8d36c96b97764feed0a0150a77e6481da0eed11
bananackck <emilly03141221@gmail.com>
2025-02-27 15:46:41 +0900

    feat : dummy data add and json matching

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -1,18 +1,23 @@
 import React, {useState} from "react";
 import "../assets/styles/MainCard.css";
+import dummy from "../assets/dummy.json";
 
+import BakeryList from "./BakeryList";
 const MainCard = () => {
+  // 하트 버튼 클릭 시 좋아요 표시
   const [liked, setLiked] = useState(false);
-
   const toggleLiked = () => {
-    // console.log("clicked");
     setLiked((prev) => !prev);
   };
+
+  // 빵집 정보
+  const bakery = dummy.bakery.at(0).info[0];
+  console.log(bakery);
+
   return (
     <div className="left-content">
       {/* 메인 콘텐츠 영역 */}
       <main >
-
             {/* 원하는 내용 삽입 (예: 빵집 목록, 정보 등) */}
             <div className="main-card">
               {/* 상단 하트 버튼 */}
@@ -22,17 +27,15 @@ const MainCard = () => {
               ></button>
               
               {/* 빵집 정보 */}
-              <h2 className="bakery-name">카테부 빵집</h2>
-              <p className="bakery-address">성남시 분당구 빵교로 00</p>
-              <p className="bakery-hours">영업 시간: 주중, 주말 9:00 ~ 9:00</p>
-              <p className="bakery-desc">식사 대용 빵이 맛있고, 조용한 분위기</p>
+              <h2 className="bakery-name">{bakery.name}</h2>
+              <p className="bakery-address">{bakery.address}</p>
+              <p className="bakery-desc">{bakery.desc}</p>
               
               {/* 태그 목록 */}
-              <p className="bakery-menu">이런 메뉴가 있어요!</p>
-              <div className="bakery-tags">
-                <span className="tag">식빵</span>
-                <span className="tag">인스타 감성</span>
-                <span className="tag">식빵</span>
+              <p className="bakery-menu">이런 곳이에요!</p>
+              <BakeryList bakery={bakery}/>
+              <div className="refresh">
+                <button className="refresh-btn">다른 빵집 보기</button>
               </div>
             </div>
 
