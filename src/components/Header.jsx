73328193c35a0886383d170b1afa45c180edb11b
doneni <xxerrenn@gmail.com>
2025-02-27 17:59:26 +0900

    feat : Add logout button and call logout func from AuthProvider - 누르면 로그아웃 함수를 호출하고 로그인 페이지로 이동하는 더미 버튼

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,20 @@ import React from "react";
 import "../assets/styles/Header.css";
 import bread1 from "../assets/images/bread1.png"; // 이미지 import
 import bread2 from "../assets/images/bread2.png"; // 이미지 import
+import logoutIcon from "../assets/images/logout.png";
+import { useAuth } from "../context/AuthContext"; // AuthContext import
+import { useNavigate } from "react-router-dom"; // 로그아웃 후 리다이렉트
 
 const Header = () => {
+  const { logout } = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
   return (
     <div >
       {/* 헤더 영역 */}
@@ -17,6 +29,14 @@ const Header = () => {
           <h1 className="header-title">빵교</h1>
         </div>
         <div className="header-right">
+          {/* 우측 원형(프로필 등) */}
+          <img
+            className="logout"
+            src={logoutIcon}// 로그아웃 아이콘
+            alt="로그아웃"
+            onClick={handleLogout} // 로그아웃 클릭 이벤트 추가
+            style={{ cursor: "pointer" }} // 클릭 가능하도록 스타일 추가
+          />
           {/* 우측 원형(프로필 등) */}
           <img
             className="circle"
