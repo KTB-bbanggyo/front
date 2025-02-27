ea8a2e803b0db2cd16c639243489a4957dab73f1
bananackck <emilly03141221@gmail.com>
2025-02-27 11:41:20 +0900

    feat : make searchpage

---
+++ src/components/SearchCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../assets/styles/SearchCard.css";
+
+const SearchCard = () => {
+  return (
+    <div className="search-card-container">
+      <div className="card">
+        <form>
+        <h2 className="title">너에 대해서 알려줘</h2>
+        <div className="search-box">dmdkd</div>
+        <textarea ></textarea>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SearchCard;
