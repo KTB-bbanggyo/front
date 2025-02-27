1383ba6997f003f2ca0ac21b252d754c4dfcf3e5
bananackck <emilly03141221@gmail.com>
2025-02-27 18:02:28 +0900

    feat : add header profile click event

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,10 @@ import bread1 from "../assets/images/bread1.png"; // 이미지 import
 import bread2 from "../assets/images/bread2.png"; // 이미지 import
 
 const Header = () => {
+  const profileClick = () => { 
+    // 프로필 클릭 시 이벤트
+    window.location.href = "http://localhost:3000/mypage"; // 마이 페이지로 이동
+  }
   return (
     <div >
       {/* 헤더 영역 */}
@@ -18,12 +22,14 @@ const Header = () => {
         </div>
         <div className="header-right">
           {/* 우측 원형(프로필 등) */}
-          <img
+          <button
             className="circle"
             src={bread2}// 빵 아이콘 위치
-            alt="빵교 로고"
+            alt="마이 페이지"
+            onClick={profileClick} // 프로필 클릭 시 이벤트
           />
         </div>
+
       </header>
     </div>
   );
