fc7c8abab7ae68821be49e9b72589d980fcf2652
doneni <xxerrenn@gmail.com>
2025-02-28 06:14:58 +0900

    Updates

---
+++ precache-manifest.a3eeb78ba600f93b2e5cc8a2b0ceab1c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398eabe8849b5494177c83d921656387",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd89aefa441dc204810a",
+    "url": "/static/css/main.1e1e4f83.chunk.css"
+  },
+  {
+    "revision": "88a8823d1809fe802b8c",
+    "url": "/static/js/2.6ee17156.chunk.js"
+  },
+  {
+    "revision": "cd89aefa441dc204810a",
+    "url": "/static/js/main.11cd6b2c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "14dddd495f462a25e832b19e47716013",
+    "url": "/static/media/backgroundBread.14dddd49.png"
+  },
+  {
+    "revision": "fad5772f41edba7b2291ca3abb436a62",
+    "url": "/static/media/bread1.fad5772f.png"
+  },
+  {
+    "revision": "7256a140cc12134dfd1608d4788009ca",
+    "url": "/static/media/bread2.7256a140.png"
+  },
+  {
+    "revision": "9f61111962cc66e53f0c907d4fcc17cb",
+    "url": "/static/media/filledHeart.9f611119.png"
+  },
+  {
+    "revision": "c55e90efee9ff090321a30c07398372e",
+    "url": "/static/media/searchBackground.c55e90ef.png"
+  }
+]);
